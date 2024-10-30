b0f7bb3076520793772390d7665cdce724af8c00
Jolimpioo <jolimpiomeloj@gmail.com>
2024-10-30 14:45:26 -0300

    Criacao inicial de Models

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,15 +1,22 @@
 const { Sequelize } = require("sequelize");
 
+// Cria uma nova instância do Sequelize para se conectar ao banco
 const sequelize = new Sequelize("nodemvc", "root", "admin123", {
   host: "localhost",
   dialect: "mysql",
 });
 
-try {
-  sequelize.authenticate();
-  console.log("Conetamos ao MySQL!");
-} catch (error) {
-  console.log(`Não foi possível conectar: ${error}`);
-}
+// Função assíncrona para conectar ao banco
+const connectDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conetamos ao MySQL!");
+  } catch (error) {
+    console.log(`Não foi possível conectar: ${error}`);
+  }
+};
 
-exports.default = sequelize;
+// Chama a função para estabelecer a conexão com o banco
+connectDatabase();
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const app = express();
-
-const connect = require("./db/dbConnect.js");
+const connect = require("./db/dbConnect");
+const Task = require("./models/Task");
 
 // Configurando o motor de template Handlebars
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
-// Middleware para processar dados de formulários
+// Middlewares para processar dados de formulários, analisar requisições em formato JSON e servir arquivos estáticos da pasta "public".
 app.use(express.urlencoded({ extended: true }));
-
-// Configura o middleware para análise de JSON nas requisições
 app.use(express.json());
-
-// Servindo arquivos estáticos
 app.use(express.static("public"));
 
-app.listen(3000);
+// Função para iniciar o servidor
+const startServer = async () => {
+  try {
+    await connect.sync(); // Sincroniza os modelos com o banco de dados
+    app.listen(3000, () => {
+      console.log("Servidor rodando na porta 3000");
+    });
+  } catch (error) {
+    console.error(`Erro ao iniciar o servidor: ${error}`);
+  }
+};
+
+// Inicia o servidor
+startServer();
